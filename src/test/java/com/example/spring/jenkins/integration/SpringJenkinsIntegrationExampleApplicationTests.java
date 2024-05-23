5e76601c71ab65de8e45cd6145d613ffd3621a6f
908192 <908192@cognizant.com>
2024-05-23 21:12:48 +0530

    firsy comimit

---
+++ src/test/java/com/example/spring/jenkins/integration/SpringJenkinsIntegrationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.jenkins.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsIntegrationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
